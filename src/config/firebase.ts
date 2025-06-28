317673390082cd7a5c824f6f40bd447ece48e845
Paul <tamas_paul@yahoo.com>
2025-06-28 19:22:11 +0300

    Final version

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,13 +1,10 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC44nrAsvC7I9yEwzyxS4AsFr3zoXnPLvw",
   authDomain: "sportsbuddy-3db70.firebaseapp.com",
@@ -17,7 +14,7 @@ const firebaseConfig = {
   appId: "1:586362534879:web:7e95caba106cf57a76226a"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 export { storage };
